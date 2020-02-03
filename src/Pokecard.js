02fb3d633ed93e5b651a5fcbe58f8c315a11e557
Altaf Quadri <altafmquadri@gmail.com>
2020-02-02 22:00:34 -0500

    added formula for padding to get higher res photos

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -4,11 +4,14 @@ import './Pokecard.css'
 class Pokecard extends Component {
     render() { 
         const {name, id, type, base_experience} = this.props.pokemon
+        const changePadding = (num) => {
+            return num.toString().padStart(3, 0)
+        }
         return ( 
             <div className='Pokemon-card'>
                 <div className='Pokemon-details'>
                     <h1>{name}</h1>
-                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}/>
+                    <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${changePadding(id)}.png`}/>
                     <p>Type: {type}</p>
                     <p>Exp: {base_experience}</p>
                 </div>
